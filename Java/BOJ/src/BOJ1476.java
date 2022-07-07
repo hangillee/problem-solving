68df3d9212c44d01bfc44fc1d909b8d4db63e825
skfcb10@naver.com <skfcb10@naver.com>
2022-07-08 04:26:40 +0900

    ADD Solve problem BOJ1476 07/08

--- Java/BOJ/src/BOJ1476.java
+++ Java/BOJ/src/BOJ1476.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.StringTokenizer;
 
-public class BOJ2873 {
+public class BOJ1476 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
