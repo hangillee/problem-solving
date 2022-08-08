26133d2a84247a466f576a191dab3556d6ad51e2
hangillee <skfcb10@naver.com>
2022-08-08 22:16:51 +0900

    ADD Solve problem BOJ25304 08/08

---
+++ Java/BOJ/src/BOJ25304.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BOJ25304 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int X = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        int total = 0;
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            total += (a * b);
+        }
+
+        if(total == X) {
+            bw.write("Yes");
+        } else {
+            bw.write("No");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
