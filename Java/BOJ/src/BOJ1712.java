52d9f783ee9879ad12469ec52d782af529c74ab0
hangillee <skfcb10@naver.com>
2022-08-13 20:05:28 +0900

    ADD Solve problem BOJ1712 08/13

---
+++ Java/BOJ/src/BOJ1712.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BOJ1712 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long C = Long.parseLong(st.nextToken());
+
+        long laptops;
+        if(B >= C) {
+            laptops = -1L;
+        } else {
+            laptops = A / (C - B) + 1;
+        }
+
+        bw.write(String.valueOf(laptops));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
