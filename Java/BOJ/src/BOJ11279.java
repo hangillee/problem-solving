73cb56579e2c9fc4aac62c888f60511bcc1f9445
hangillee <skfcb10@naver.com>
2022-09-07 12:41:45 +0900

    ADD Solve problem BOJ11279 09/07

---
+++ Java/BOJ/src/BOJ11279.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class BOJ11279 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        int N = Integer.parseInt(br.readLine());
+        for(int i = 0; i < N; i++) {
+            int command = Integer.parseInt(br.readLine());
+            if(pq.isEmpty() && command == 0) {
+                bw.write("0\n");
+            } else if (command == 0) {
+                bw.write(pq.poll() + "\n");
+            } else {
+                pq.add(command);
+            }
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
