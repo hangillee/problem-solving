b383260342b649faf10aca3b15079a16429db233
hangillee <skfcb10@naver.com>
2022-09-30 12:09:13 +0900

    ADD Solve problem BOJ14652 09/30

---
+++ Java/BOJ/src/BOJ14652.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BOJ14652 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int row = K / M;
+        int col = K % M;
+
+        bw.write(row + " " + col + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
