847e694bd02d2d871a4ef723d8f06c7369d478a4
hangillee <skfcb10@naver.com>
2022-11-13 16:27:23 +0900

    ADD Solve problem BOJ21300

---
+++ Java/BOJ/src/BOJ21300.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BOJ21300 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int total = 0;
+        for(int i = 0; i < 6; i++) {
+            total += Integer.parseInt(st.nextToken()) * 5;
+        }
+        bw.write(String.valueOf(total));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
