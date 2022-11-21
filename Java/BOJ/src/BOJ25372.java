8b55188d5dec644a1111efc78a359a456a8b3435
hangillee <skfcb10@naver.com>
2022-11-21 15:11:02 +0900

    ADD Solve problem BOJ25372 11/21

---
+++ Java/BOJ/src/BOJ25372.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class BOJ25372 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        for(int i = 0; i < N; i++) {
+            String password = br.readLine();
+            if(password.length() >= 6 && password.length() <= 9) {
+                bw.write("yes\n");
+            } else {
+                bw.write("no\n");
+            }
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
