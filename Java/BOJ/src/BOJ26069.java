db8fb369d5dff589261cb3e100964f7ece41ac85
hangillee <skfcb10@naver.com>
2022-11-28 15:13:14 +0900

    ADD Solve problem BOJ26069 11/28

--- Java/BOJ/src/BOJ26069.java
+++ Java/BOJ/src/BOJ26069.java
@@ -1,38 +1,32 @@
 import java.io.*;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
-public class BOJGomGomB {
+public class BOJ26069 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
-
-        boolean chongchongMeet = false;
-        int count = 1;
-        String lastMeet = "";
+        HashMap<String, Integer> meet = new HashMap<>();
         for(int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             String A = st.nextToken();
             String B = st.nextToken();
-            if(!(A.equals(lastMeet)) && !(B.equals(lastMeet))) {
-                chongchongMeet = false;
-            }
             if(A.equals("ChongChong") || B.equals("ChongChong")) {
-                chongchongMeet = true;
-                if(!(A.equals("ChongChong"))) {
-                    lastMeet = A;
-                } else {
-                    lastMeet = B;
-                }
+                meet.put(A, 1);
+                meet.put(B, 1);
+            } else if(meet.getOrDefault(A, 0) == 1 || meet.getOrDefault(B, 0) == 1) {
+                meet.put(A, 1);
+                meet.put(B, 1);
+            } else {
+                meet.put(A, 0);
+                meet.put(B, 0);
             }
-            if(chongchongMeet) {
-                if(A.equals(lastMeet)) {
-                    lastMeet = B;
-                } else {
-                    lastMeet = A;
-                }
-                count++;
-            }
+        }
+
+        int count = 0;
+        for(int value : meet.values()) {
+            count += value;
         }
 
         bw.write(String.valueOf(count));
