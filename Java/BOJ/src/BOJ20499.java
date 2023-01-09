943322f9d7fb5bbd6d5a1cff327e5bb94dfdd5be
hangillee <skfcb10@naver.com>
2023-01-09 23:59:52 +0900

    ADD Solve problem BOJ20499 01/09

---
+++ Java/BOJ/src/BOJ20499.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BOJ20499 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), "/");
+        int K = Integer.parseInt(st.nextToken());
+        int D = Integer.parseInt(st.nextToken());
+        int A = Integer.parseInt(st.nextToken());
+        if(K + A < D || D == 0) {
+            bw.write("hasu");
+        } else {
+            bw.write("gosu");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
