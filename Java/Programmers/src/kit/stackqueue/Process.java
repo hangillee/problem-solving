403909610d861902394e5adbf272e6cbf10f98f6
hangillee <coderoad.io@gmail.com>
2025-03-27 20:40:54 +0900

    feat: 코딩테스트지도 4주차 과제 풀이 - 프로그래머스 프로세스 2차 제출

--- Java/Programmers/src/kit/stackqueue/Process.java
+++ Java/Programmers/src/kit/stackqueue/Process.java
@@ -1,8 +1,6 @@
 package kit.stackqueue;
 
 import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Queue;
 
 public class Process {
@@ -17,27 +15,26 @@ class ProcessSolution {
     public int solution(int[] priorities, int location) {
         int answer = 1;
 
-        List<Integer> prioritiesList = new ArrayList<>();
+        Queue<Integer> processes = new ArrayDeque<>();
         for (int priority : priorities) {
-            prioritiesList.add(priority);
+            processes.add(priority);
         }
 
-        Queue<Integer> processes = new ArrayDeque<>(prioritiesList);
         while (!processes.isEmpty()) {
             Integer maxPriority = processes.stream()
                     .max(Integer::compare)
-                    .get();
+                    .get(); // 최우선 프로세스 탐색
 
             Integer process = processes.remove();
-            if (process < maxPriority) {
-                processes.add(process);
+            if (process < maxPriority) { // 최우선 프로세스가 아니면
+                processes.add(process); // 다시 큐에 삽입
                 location = location == 0 ? processes.size() - 1 : location - 1;
-            } else {
-                if (location == 0) {
-                    break;
-                } else {
-                    location--;
-                    answer++;
+            } else { //최우선 프로세스라면
+                if (location == 0) { // 또한 현재 프로세스의 실행 순서를 구하고 있다면
+                    break; // 탐색 종료
+                } else { // 찾고 있던 프로세스가 아니라면
+                    location--; // 포인터 이동
+                    answer++; // 실행된 프로세스 개수 증가
                 }
             }
         }
