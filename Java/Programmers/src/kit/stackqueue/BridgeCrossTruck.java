b4dd85a299916a9f020e2b1d9593e556a50cc55c
hangillee <coderoad.io@gmail.com>
2025-03-28 15:38:13 +0900

    feat: 코딩테스트지도 4주차 과제 풀이 - 프로그래머스 다리를 지나는 트럭 2차 제출

--- Java/Programmers/src/kit/stackqueue/BridgeCrossTruck.java
+++ Java/Programmers/src/kit/stackqueue/BridgeCrossTruck.java
@@ -13,19 +13,24 @@ public class BridgeCrossTruck {
 class BridgeCrossTruckSolution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         int answer = 0;
-        Deque<Integer> bridge = new ArrayDeque<>();
         int currentWeight = 0;
+        Deque<Integer> bridge = new ArrayDeque<>();
 
-        for (int i = 0; i < truck_weights.length; i++) {
-            if (currentWeight + truck_weights[i] <= weight && bridge.size() < bridge_length) {
+        for (int i = 0; i < bridge_length; i++) {
+            bridge.push(0);
+        }
+
+        for (int i = 0; i < truck_weights.length;) {
+            answer++;
+            currentWeight -= bridge.remove();
+            if (currentWeight + truck_weights[i] <= weight) {
                 bridge.add(truck_weights[i]);
                 currentWeight += truck_weights[i];
+                i++;
             } else {
-                currentWeight -= bridge.remove();
-                i--;
+                bridge.add(0);
             }
-            answer++;
         }
-        return answer + bridge.size();
+        return answer + bridge_length;
     }
 }
