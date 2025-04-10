832b1b622cb9002d27bf1379a02ef8c34f06bc27
hangillee <coderoad.io@gmail.com>
2025-04-11 01:42:25 +0900

    feat: 코딩테스트지도 6주차 과제 풀이 - 1655번 가운데를 말해요 3차 제출

--- Java/BOJ/src/BOJ1655/Main.java
+++ Java/BOJ/src/BOJ1655/Main.java
@@ -5,8 +5,6 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.PriorityQueue;
 
 public class Main {
@@ -16,28 +14,25 @@ public class Main {
     public static void main(String[] args) throws IOException {
         int N = Integer.parseInt(br.readLine());
 
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
-        List<Integer> temp = new LinkedList<>();
-        for (int i = 0; i < N; i++) {
-            queue.add(Integer.parseInt(br.readLine()));
+        PriorityQueue<Integer> minPriorityQueue = new PriorityQueue<>();
+        PriorityQueue<Integer> maxPriorityQueue = new PriorityQueue<>((o1, o2) -> o2 - o1);
 
-            if (queue.size() == 1) {
-                bw.write(queue.peek() + "\n");
-                continue;
-            }
-            int currentSize = queue.size();
-            if (queue.size() % 2 == 0) {
-                for (int j = 0; j < currentSize / 2 - 1; j++) {
-                    temp.add(queue.poll());
-                }
+        for (int i = 0; i < N; i++) {
+            int number = Integer.parseInt(br.readLine());
+            if (maxPriorityQueue.size() == minPriorityQueue.size()) {
+                maxPriorityQueue.add(number);
             } else {
-                for (int j = 0; j < currentSize / 2; j++) {
-                    temp.add(queue.poll());
+                minPriorityQueue.add(number);
+            }
+
+            if (!maxPriorityQueue.isEmpty() && !minPriorityQueue.isEmpty()) {
+                if (maxPriorityQueue.peek() > minPriorityQueue.peek()) {
+                    int temp = maxPriorityQueue.poll();
+                    maxPriorityQueue.add(minPriorityQueue.poll());
+                    minPriorityQueue.add(temp);
                 }
             }
-            bw.write(queue.peek() + "\n");
-            queue.addAll(temp);
-            temp.clear();
+            bw.write(maxPriorityQueue.peek() + "\n");
         }
 
         bw.flush();
